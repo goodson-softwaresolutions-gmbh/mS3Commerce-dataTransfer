64c8c2eb666aed5e267db8adc072d60d750f6a8f
Philip Masser <philip.masser@goodson.at>
2023-03-15 16:21:22 +0100

    Bump version to 6.6.3.23110

--- mS3CommerceVersion.php
+++ mS3CommerceVersion.php
@@ -13,5 +13,5 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-define('MS3C_VERSION', '6.6.1.23100');
-define('MS3C_VERSION_FULL', '6.6.1.23100');
+define('MS3C_VERSION', '6.6.3.23110');
+define('MS3C_VERSION_FULL', '6.6.3.23110');
